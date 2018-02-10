99fb4bbe68e3fad959a49dfa6f3de23b19cf65e9
Kurchevskiy <kurchevskiy.aleksandr@ya.ru>
2018-02-10 10:13:03 +0600

    Add shot

---
+++ shot.rb
@@ -0,0 +1,6 @@
+class Shot
+	@enemy_ship = [4, 5, 6, 12, 9]
+	def shot 
+
+	end
+end
--- use_ship.rb
+++ use_ship.rb
@@ -2,10 +2,10 @@ require "./ship"
 begin
 	ship = Ship.new false, 4
 
-	# p ship.is_stranger
-	p ship.ship_size
-	p ship.status
-	ship.state = true
+	p ship.is_stranger # Если true, то чужой. Если false, то свой
+	p ship.ship_size   # Размер кораблика
+	p ship.status      # состоянии кораблика
+	ship.state = true  # изменение состояния
 	p ship.status
 	ship.state = false
 	p ship.status
